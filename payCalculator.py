fd21d38721f3725d4357be89545769f4239a0fd5
Nathan Gearke <Nathan.Gearke@vu.com>
2022-01-28 11:10:27 -0600

    BREAK EM!

--- payCalculator.py
+++ payCalculator.py
@@ -1,23 +1,13 @@
 def calculatePay():
-    
     # This first line is provided for you
     pay = 0.0
-    try:
-        hrs = float(input("Enter Hours: "))
-        rate = float(input("Enter Rate: "))
-    except:
-        print("Error, please enter numeric input")
+    hrs = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
 
 
-    if hrs > 40.0:
-        overtime = hrs-40
-        overtime *= (rate*1.5)
-        pay = (40*rate)+overtime
-        print ("Pay: " + str(pay))
-    else:
-        pay = hrs*rate
-        print ("Pay: " + str(pay))
-        
+    pay = hrs*rate
+    print ("Pay: " + str(pay))
+
 # end assignment
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
